21440467388747f6434e43770e274aad7e216ef1
Anartz Mugika Ledo <mugan86@gmail.com>
2024-12-16 17:07:17 +0100

    Auto-generated commit

---
+++ chunk-VKN53E4X.js
@@ -0,0 +1,1 @@
+import{Ja as t,Ka as a,Oa as p,ta as o}from"./chunk-PR3FXTTH.js";var s=class n{static \u0275fac=function(e){return new(e||n)};static \u0275cmp=o({type:n,selectors:[["app-page-not-found"]],decls:2,vars:0,template:function(e,m){e&1&&(t(0,"p"),p(1,"page-not-found works!"),a())},encapsulation:2})};export{s as PageNotFoundComponent};
